2d5ba9e40b505f8123608c8fdf5a0a89d0e87b6b
Tom Hart <kitsonian@gmail.com>
2022-06-03 09:19:17 -0400

    Slightly faster version using a hash

--- 2068. Check Whether Two Strings are Almost Equivalent.js
+++ 2068. Check Whether Two Strings are Almost Equivalent.js
@@ -8,33 +8,18 @@
 * @param {string} word2
 * @return {boolean}
 */
-const count = (needle, haystack) => haystack.filter(item => item === needle).length
 var checkAlmostEquivalent = function (word1, word2) {
-    // make a unique set of letters
-    const firstWord = word1.split('');
-    const secondWord = word2.split('');
-    const uniques = [...new Set([...firstWord, ...secondWord])]
-    // loop the letters and check the frequency of occurence in both words
-    for( let i = 0; i <= uniques.length; i++ ) {
-        const a = count(uniques[i], firstWord);
-        const b = count(uniques[i], secondWord);
-        // return false if the count is greater than 3
-        if (( a - b ) > 3 || ( b - a ) > 3) return false
+    const hash = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0}
+    for ( let i = 0; i <= word1.length - 1; i++ ){
+        hash[word1[i]] += 1
+    }
+    for ( let i = 0; i <= word2.length - 1; i++ ){
+        hash[word2[i]] -= 1
+    }
+    const letters = Object.keys(hash);
+    for ( let i = 0; i <= letters.length - 1; i++ ) {
+        if ( hash[letters[i]] > 3 || hash[letters[i]] < -3 ) return false
     }
     return true
 };
-checkAlmostEquivalent('aaaa', 'bccb') //?
-checkAlmostEquivalent("abcdeef", "abaaacc") //? 
-checkAlmostEquivalent("cccddabba", "babababab") //? 
-checkAlmostEquivalent("zzzyyy", "iiiiii") //? 
-
-// const charCode = char => char.charCodeAt()
-// var checkAlmostEquivalent2 = function (word1, word2) {
-//  // find the unique characters between both strings
-//  // making the strings lowercase will help return a consistent character code later
-//  // sorting is important to avoid a negative decider
-//  const uniques = [...new Set([...word1.toLowerCase().split(''), ...word2.toLowerCase().split('')])].sort();
-//  uniques //?
-//  const decider = uniques.reduce(( previousValue, currentValue, i ) => charCode(uniques[i]) - charCode(uniques[i-1] || uniques[0]), uniques[0]);
-//  return decider > 3 ? true : false;
-// };
+checkAlmostEquivalent('aaaa', 'bccb')
